80ebd026fcddca2f43a8753e9ca48f738c05640a
ThomasMLdeJong <ThomasMLdeJong@gmail.com>
2024-04-07 17:39:11 +0200

    made a seperate functions file for the webapp

--- app/webapp/application.py
+++ app/webapp/application.py
@@ -1,109 +1,20 @@
-import os
 import logging
-import bcrypt
-import psycopg2
 from flask import Flask, session, redirect, url_for, request, render_template, flash
-from kubernetes import client, config
-import base64
+from functions import register_new_user, authenticate, get_secret, is_authenticated
 
 app = Flask(__name__)
 app.logger.setLevel(logging.INFO)
 
-def is_authenticated():
-    """
-    Controleer of de user geautenticeerd is.
-    """
-    return "username" in session
-
-
-def get_secret(namespace, secret_name): 
-    config.load_incluster_config()
-    v1 = client.CoreV1Api()
-
-    try: 
-        secret = v1.read_namespaced_secret(name=secret_name, namespace=namespace)
-
-        secret_data = {}
-        for key, value in secret.data.items():
-            secret_data[key] = base64.b64decode(value).decode('utf-8')
-
-        return secret_data
-    except Exception as e: 
-        print(f"Error fetching secret: {e}")
-        return None
-
-def get_database_credentials():
-    """
-    Opvragen van database credentials
-    """
-    return {
-        "dbname": os.getenv("DB_NAME"),
-        "user": os.getenv("DB_USER"),
-        "password": os.getenv("DB_PASSWORD"),
-        "host": os.getenv("DB_HOST"),
-        "port": os.getenv("DB_PORT", "5432")
-    }
-
-def authenticate(username, password):
-    """ 
-    controleer of de username en password die worden gegeven door de gebruiker,
-    overeen komen met dat wat in de database staat.
-    """
-    try:
-        db_credentials = get_database_credentials()
-
-        connection = psycopg2.connect(**db_credentials)
-        cursor = connection.cursor()
-
-        query = "SELECT * FROM users WHERE username = %s"
-        cursor.execute(query, (username,))
-        user = cursor.fetchone()
-
-        if user:
-            if bcrypt.checkpw(password.encode('utf-8'), user[2].encode('utf-8')):
-                app.logger.info(f"The user '{username}' logged in successfully")
-                session["username"] = username
-                return True
-            else:
-                app.logger.warning(f"Invalid password for user '{username}'")
-                return False
-        else:
-            app.logger.warning(f"User '{username}' not found")
-            return False
-    finally:
-        cursor.close()
-        connection.close()
-
-def register_new_user(username, password):
-    """ 
-    Registreer een nieuwe gebruiker
-    """
-    try:
-        db_credentials = get_database_credentials()
-        connection = psycopg2.connect(**db_credentials)
-        cursor = connection.cursor()
-
-        hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
-
-        cursor.execute("INSERT INTO users (username, hashed_password) VALUES (%s, %s)", (username, hashed_password.decode('utf-8')))
-        connection.commit()
-        app.logger.info(f"User '{username}' registered successfully")
-        return True
-    finally:
-        cursor.close()
-        connection.close()
-
-
 @app.route("/")
 def index():
-    return render_template("index.html", is_authenticated=is_authenticated())
+    return render_template("index.html", is_authenticated=is_authenticated(session))
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
         username = request.form.get("username")
         password = request.form.get("password")
-        if authenticate(username, password):
+        if authenticate(username, password, session, app):
             return redirect(url_for("index"))
         else:
             flash("Invalid username or password", "error")
@@ -119,7 +30,7 @@ def register_user():
     if request.method == "POST":
         username = request.form.get("username")
         password = request.form.get("password")
-        if register_new_user(username, password):
+        if register_new_user(username, password, app):
             return redirect(url_for("login"))
         else:
             return render_template("register.html", error="Failed to register user")
